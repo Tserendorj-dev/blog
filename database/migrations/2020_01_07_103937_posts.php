c1295f01a0abdb2fbfa2060904c63c86b9afdf5a
Tserendorj Chuluunbat <Tserendorj>
2020-01-07 19:49:29 +0900

    not relation seed update

--- database/migrations/2020_01_07_103937_posts.php
+++ database/migrations/2020_01_07_103937_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Categories extends Migration
+class Posts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,17 @@ class Categories extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('cat_id');
-            $table->string('cat_name');
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->integer('cat_id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('pic_path');
+            $table->longText('desc_text');
+            $table->longText('full_text');
             $table->boolean('is_visible')->default(0);
+            $table->integer('views');
+            $table->integer('comments');
             $table->string('lang');
             $table->timestamps();
         });
