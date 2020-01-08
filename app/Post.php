9ac6bbd392b2968a840181426fad74d567e7660d
Tserendorj Chuluunbat <Tserendorj>
2020-01-08 22:28:49 +0900

    model update

--- app/Post.php
+++ app/Post.php
@@ -9,7 +9,7 @@ class Post extends Model
     protected $table = "posts";
     protected $fillable = ['title', 'pic_path', 'desc_text','full_text','is_visible','views','comments','lang'];
 
-    public function comments()
+    public function comment()
     {
         return $this->hasMany(Comment::class);
     }
