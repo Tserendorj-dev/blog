22cf3b5670b96e271c79500e4bbaf841ab1c5b96
Tserendorj Chuluunbat <Tserendorj>
2020-01-12 13:43:41 +0900

    view

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,8 +1,19 @@
 @extends('admin.layouts.main')
 
 @section('content')
-<table>
-<tr><td>name</td></tr>
-<tr><td></td></tr>
-</table>
+<div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">Сайтын удирдлага</h1>
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                    <table>
+                    <tr><td>Category Name</td></tr>
+                    </table>
+                    </div>
+</div>
+<!-- /.container-fluid -->
 @endsection
